d53d236bf79e1321a598f9c9acf0423cb7ed909f
Maxime Guerois <maximeguerois@gmail.com>
2021-11-09 17:09:07 +0100

    exercice 2

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2021_11_09_105458) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.string "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
